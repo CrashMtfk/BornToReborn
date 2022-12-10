7a6811eb5b1f5c73908f993a2b2703fbd74c70b2
Oprea-Mihai-30127 <mihairobert05@yahoo.com>
2022-12-11 00:29:41 +0200

    1230 Login solved
    First Update

---
+++ Backend/src/main/java/CSB/BornToReborn/BornToRebornApplication.java
@@ -0,0 +1,13 @@
+package CSB.BornToReborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BornToRebornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BornToRebornApplication.class, args);
+	}
+
+}
