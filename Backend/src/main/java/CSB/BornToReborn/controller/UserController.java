fdd4d03b68c95e86edea7da935326d1dd787e6b2
Oprea-Mihai-30127 <mihairobert05@yahoo.com>
2022-12-11 02:47:50 +0200

    Update for backend connection
    Backend up to date with Frontend for page connections;

--- Backend/src/main/java/CSB/BornToReborn/controller/UserController.java
+++ Backend/src/main/java/CSB/BornToReborn/controller/UserController.java
@@ -19,29 +19,47 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/register")
-    public String getRegisterPage(Model model){
-        model.addAttribute("registerRequest",new UsersModel());
-        return "register_page";
+    @GetMapping("/signup")
+    public String getSignupPage(Model model){
+        model.addAttribute("signupRequest",new UsersModel());
+        return "signup";
     }
 
     @GetMapping("/login")
     public String getLoginPage(Model model){
         model.addAttribute("loginRequest",new UsersModel());
-        return "login_page";
+        return "login";
     }
 
     @GetMapping("/index")
-    public String getIndex(Model model){
+    public String getIndexPage(Model model){
         model.addAttribute("indexRequest",new UsersModel());
-        return "index_page";
+        return "index";
     }
 
-    @PostMapping("/register")
-    public String register(@ModelAttribute UsersModel usersModel) {
-        System.out.println("Register request: " + usersModel);
+    @GetMapping("/recycle_points")
+    public String getRecyclePointsPage(Model model) {
+        model.addAttribute("recyclePointsRequest",new UsersModel());
+        return "recycle_points";
+    }
+
+    @GetMapping("/profile")
+    public String getProfilePage(Model model) {
+        model.addAttribute("profileRequest",new UsersModel());
+        return "profile";
+    }
+
+    @GetMapping("/articles")
+    public String getArticlesPage(Model model){
+        model.addAttribute("articlesRequest",new UsersModel());
+        return "articles";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute UsersModel usersModel) {
+        System.out.println("Sign-up request: " + usersModel);
         UsersModel registeredUser = userService.registerUser(usersModel.getFullName(),usersModel.getUsername(),usersModel.getEmail(),usersModel.getPassword(),usersModel.getCountry(),usersModel.getAge(),usersModel.getGender());
-        return registeredUser == null ? "register_page" : "login_page";
+        return registeredUser == null ? "signup" : "login";
     }
 
     @PostMapping("/login")
@@ -50,10 +68,10 @@ public class UserController {
         UsersModel authenticated = userService.authenticate(usersModel.getUsername(),usersModel.getPassword());
         if(authenticated != null) {
             model.addAttribute("userName",authenticated.getUsername());
-            return "profile_page";
+            return "profile";
         }
         else {
-            return "login_page";
+            return "login";
         }
     }
 }
